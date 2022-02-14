1c3e2ae89fe08707cd17ce758c8d4c1f28b9a682
Alex Martynov <martynov.alex@gmail.com>
2022-02-14 18:12:26 +0600

    Get and show exchange rate

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,31 +1,45 @@
-const List<String> currenciesList = [
-  'AUD',
-  'BRL',
-  'CAD',
-  'CNY',
-  'EUR',
-  'GBP',
-  'HKD',
-  'IDR',
-  'ILS',
-  'INR',
-  'JPY',
-  'MXN',
-  'NOK',
-  'NZD',
-  'PLN',
-  'RON',
-  'RUB',
-  'SEK',
-  'SGD',
-  'USD',
-  'ZAR'
-];
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-const List<String> cryptoList = [
-  'BTC',
-  'ETH',
-  'LTC',
-];
+const apiKey = '6E802769-3B00-4133-9965-0E0BA9268258';
+const coinAPIMapURL = 'https://rest.coinapi.io/v1/exchangerate';
+// Пример запроса: https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=6E802769-3B00-4133-9965-0E0BA9268258
 
-class CoinData {}
+const Map<String, String> currenciesMap = {
+  'RUB': 'Russian Ruble',
+  'USD': 'US Dollar',
+  'EUR': 'Euro',
+  'GBP': 'British Pound',
+  'CAD': 'Canadian Dollar',
+  'AUD': 'Australian Dollar',
+  'NZD': 'New Zealand Dollar',
+  'JPY': 'Japanese Yen',
+};
+
+const Map<String, String> cryptoMap = {
+  'BTC': 'Bitcoin',
+  'ETH': 'Ethereum',
+  'SOL': 'Solana',
+  'DOGE': 'Dogecoin',
+  'SHIB': 'Shiba Inu',
+};
+
+class CoinData {
+  Future<double> getExchangeRate(
+      {required String currency, required String crypto}) async {
+    double exchangeRate = 0.0;
+
+    var uri = Uri.parse('$coinAPIMapURL/$crypto/$currency?apikey=$apiKey');
+    http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      exchangeRate = jsonDecode(data)['rate'];
+      print(exchangeRate);
+      return exchangeRate;
+    } else {
+      print(response.statusCode);
+    }
+    return 0.0;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -9,67 +9,86 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = 'AUD';
-  int selectedValue = 0;
+  // Создаем объект класса CoinData, он содержит метод, который запрашивает
+  // данные по курсу для выбранной пары
+  CoinData coinData = CoinData();
 
-  Container androidDropdown() {
+  // Стартовый выбор тикеров
+  String selectedCurrencyTicker = 'USD';
+  String selectedCryptoTicker = 'BTC';
+
+  // Курс обмена
+  double exchangeRate = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Получаем данные при загрузке приложения
+    Future.delayed(Duration.zero, () async {
+      exchangeRate = await coinData.getExchangeRate(
+          currency: selectedCurrencyTicker, crypto: selectedCryptoTicker);
+
+      // Как только данные получены, передаем их в функцию обновления UI
+      updateUI(exchangeRate);
+    });
+  }
+
+  // Функция обновления параметров UI
+  void updateUI(
+    double exchangeRateUpdate,
+  ) {
+    setState(() {
+      exchangeRate = exchangeRateUpdate;
+    });
+  }
+
+  Container androidDropdown(Map<String, String> itemsMap, String pickerType) {
     List<DropdownMenuItem<String>> androidDropdownItems = [];
+    String selectedTicker = '';
 
-    for (String currency in currenciesList) {
-      var newItem = DropdownMenuItem(child: Text(currency), value: currency);
+    for (var currency in itemsMap.entries) {
+      var newItem = DropdownMenuItem(
+          child: Text('${currency.value} [${currency.key}]'),
+          value: currency.key);
       androidDropdownItems.add(newItem);
     }
+
+    switch (pickerType) {
+      case 'currency':
+        selectedTicker = selectedCurrencyTicker;
+        break;
+      case 'crypto':
+        selectedTicker = selectedCryptoTicker;
+        break;
+    }
+
     return Container(
-      width: 120,
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      margin: EdgeInsets.only(top: 15, left: 15, right: 15),
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.white, width: 2),
         borderRadius: BorderRadius.circular(12),
       ),
       child: DropdownButtonHideUnderline(
         child: DropdownButton<String>(
-          value: selectedCurrency,
-          iconSize: 20,
+          value: selectedTicker,
+          iconSize: 30,
           icon: const Icon(
-            Icons.arrow_downward_rounded,
+            Icons.arrow_downward,
             color: Colors.white,
           ),
-          // elevation: 16,
           isExpanded: true,
-          style: const TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.white, fontSize: 20),
           items: androidDropdownItems,
-          // Ниже более простое решение в соотвествии с документацией
-          // на DropdownButton class
-          // items: currenciesList
-          //     .map((String item) => DropdownMenuItem(
-          //           value: item,
-          //           child: Text(item),
-          //         ))
-          //     .toList(),
-          onChanged: (value) => setState(() => selectedCurrency = value!),
+          onChanged: (value) => setState(() {
+            if (pickerType == 'currency') {
+              selectedCurrencyTicker = value!;
+            } else if (pickerType == 'crypto') {
+              selectedCryptoTicker = value!;
+            }
+          }),
         ),
       ),
-    );
-  }
-
-  CupertinoPicker iOSPicker() {
-    List<Text> iOSPickerItems = [];
-
-    for (String item in currenciesList) {
-      iOSPickerItems
-          .add(Text(item, style: TextStyle(color: Colors.white, fontSize: 26)));
-    }
-
-    return CupertinoPicker(
-      squeeze: 1.3,
-      useMagnifier: true,
-      magnification: 1.1,
-      itemExtent: 32.0,
-      onSelectedItemChanged: (selectedIndex) {
-        selectedValue = selectedIndex;
-        //print(currenciesList[selectedValue]);
-      },
-      children: iOSPickerItems,
     );
   }
 
@@ -88,7 +107,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: const Color(0xFF03F4C6),
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -96,22 +115,48 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 $selectedCryptoTicker = ${exchangeRate.toStringAsFixed(6)} $selectedCurrencyTicker',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
           ),
           Container(
-            height: 150.0,
             alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
+            padding: const EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                androidDropdown(currenciesMap, 'currency'),
+                androidDropdown(cryptoMap, 'crypto'),
+                const SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () async {
+                    exchangeRate = await coinData.getExchangeRate(
+                        currency: selectedCurrencyTicker,
+                        crypto: selectedCryptoTicker);
+                    updateUI(exchangeRate);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    textStyle:
+                        const TextStyle(fontSize: 20, color: Colors.black),
+                    primary: const Color(0xFF0331F4),
+                    elevation: 0,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Text('REQUEST'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
